8543973e49557471afb1e4f562395170fcbfa30e
RazielG <razielgmoncayo@gmail.com>
2026-06-22 20:30:50 -0600

    feat: task function with a basic onClick attribute working

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react"
 
-export default function Task( { title } ) {
-    return <h1>{title}</h1>
+
+//? State -> Even handling -> Conditional rendering
+
+// this funtion is a component and show a title and a button 
+export default function Task( { title } ) { // {title} = prop 
+
+    // State declaration or set up state or the component state
+    const [completed, setCompleted] = useState(false) //*Component State
+    //* completed = variable
+    //* setCompleted = setter 
+
+    return (
+        <>
+            <h1>{title}</h1> {/* prop used */}
+            <button onClick={() =>{   // onClick = React Special Atribute - Even handling
+                setCompleted(!completed) // update the complete value 
+            }}>
+                {(completed) ? "Completed" : "Click me"} {/* button text according the completed value - Contitional rendering  */}
+            </button> 
+        </>
+    )
 }
