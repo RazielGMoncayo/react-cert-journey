953fcf938a6925edbc52439fcd5bff9da7c6af07
RazielG <razielgmoncayo@gmail.com>
2026-06-24 12:15:55 -0600

    refactor: create handlers section

--- frontend/src/components/TitleInput.jsx
+++ frontend/src/components/TitleInput.jsx
@@ -4,16 +4,24 @@ export default function TitleInput() {
 
     const [title, setTitle] = useState("")
 
+    //* Handlers - named function declaration 
+
+    const handleSubmit = (e) => {
+        e.preventDefault() //  Prevents the default form submission and page reload
+        console.log("Submitted title: ", title)
+    }
+
+    const handleChange = (e) => {
+        setTitle(e.target.value) // e represents the input change event
+    }
+
     return (
         <>
-            <form onSubmit={ e => {
-                e.preventDefault() //  Prevents the default form submission and page reload
-                console.log("Submitted title: ", title)
-            } }>                
+            <form onSubmit={handleSubmit}>                
                 <input 
                     type="text" 
                     value={title} 
-                    onChange={ e => setTitle(e.target.value) }/> {/* e represents the input change event */}
+                    onChange={handleChange}/>
                 <p>{title}</p>
                 <button>Create Task</button>
             </form>
