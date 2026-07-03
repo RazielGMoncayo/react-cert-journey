01b024474f84ad481bc97696ae880e8c43caa847
RazielG <razielgmoncayo@gmail.com>
2026-07-02 18:13:31 -0600

    doc: add comments in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//? Creates the React root and connects the app to the HTML element with id="root".
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* Development tool that helps detect potential problems by performing extra checks. */}
     <App />
   </StrictMode>,
 )
